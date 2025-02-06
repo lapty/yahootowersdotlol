f4d540e5d06319e77a0a9b4b1d15aee5d778caee
Charles Nguyen <charlesnguyen@Charless-MacBook-Pro.local>
2025-02-06 09:38:37 -0500

    halftone styles, updated border width for header, updated header text

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "it's not tricky towers...";
-export const SITE_DESCRIPTION = 'Yahoo Towers laugh out loud';
+export const SITE_DESCRIPTION = 'yahoo towers dot laugh out loud';
